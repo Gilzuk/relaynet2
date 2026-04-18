a44083b301d212ce09425f144de08e1694e9161f
Gil Zukerman <gzukerma@mail.tau.ac.il>
2026-04-18 21:39:15 +0300

    fixing compilation errors after sync to overleaf

--- __overleaf_thesis (1)/read_readme.py
+++
@@ -1,5 +0,0 @@
-with open(r'C:\Users\gzukerma\.vscode\extensions\iamhyc.overleaf-workshop-0.15.8\README.md', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-with open('overleaf_readme.txt', 'w', encoding='utf-8') as out:
-    out.write(text)
